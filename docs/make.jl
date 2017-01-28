4f7b230658ecafafb96b56da74040d44fe887833
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2017-01-28 10:31:29 -0200

    Update docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,11 +2,18 @@ using Documenter
 using LinearOperators
 
 makedocs(
-  modules = [LinearOperators]
+  modules = [LinearOperators],
+  assets = ["assets/style.css"],
+  format = :html,
+  sitename = "LinearOperators.jl",
+  pages = Any["Home" => "index.md",
+              "Tutorial" => "tutorial.md",
+              "Reference" => "reference.md"]
 )
 
-deploydocs(deps = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
+deploydocs(deps = nothing, make = nothing,
   repo = "github.com/JuliaSmoothOptimizers/LinearOperators.jl.git",
-  julia = "release",
+  target = "build",
+  julia = "0.5",
   latest = "master"
 )
