193655cb3431fcc425e3b5380d19b9ba1933394d
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2017-12-10 18:20:11 -0200

    Change kron implementation

--- src/kron.jl
+++ src/kron.jl
@@ -1,56 +1,29 @@
-export opKron
+import Base.kron
 
-function opKron(A :: AbstractMatrix,
-                B :: Union{AbstractMatrix, AbstractLinearOperator})
+# (A ⊗ B)×vec(X) = vec(BXAᵀ)
+function kron(A :: AbstractLinearOperator, B :: AbstractLinearOperator)
   m, n = size(A)
   p, q = size(B)
   T = promote_type(eltype(A), eltype(B))
   function prod(x)
-    S = promote_type(T, eltype(x))
-    opx = zeros(S, m * p)
-    Bx = zeros(S, p)
-    for j = 1:n
-      @views Bx .= B * x[(j - 1) * q + (1:q)]
-      for i = 1:m
-        aij = A[i,j]
-        if aij != 0
-          @views opx[(i - 1) * p + (1:p)] .+= aij * Bx
-        end
-      end
-    end
-    return opx
+    X = reshape(x, q, n)
+    return full(B * X * A.')[:]
   end
   function tprod(x)
-    S = promote_type(T, eltype(x))
-    opx = zeros(S, n * q)
-    Btx = zeros(S, q)
-    for i = 1:m
-      @views Btx .= B.' * x[(i - 1) * p + (1:p)]
-      for j = 1:n
-        aij = A[i,j]
-        if aij != 0
-          @views opx[(j - 1) * q + (1:q)] .+= aij * Btx
-        end
-      end
-    end
-    return opx
+    X = reshape(x, p, m)
+    return full(B.' * X * A)[:]
   end
   function ctprod(x)
-    S = promote_type(T, eltype(x))
-    opx = zeros(S, n * q)
-    Btx = zeros(S, q)
-    for i = 1:m
-      @views Btx .= B' * x[(i - 1) * p + (1:p)]
-      for j = 1:n
-        aij = A[i,j]'
-        if aij != 0
-          @views opx[(j - 1) * q + (1:q)] .+= aij * Btx
-        end
-      end
-    end
-    return opx
+    X = reshape(x, p, m)
+    return full(B' * X * conj(A))[:]
   end
   symm = issymmetric(A) && issymmetric(B)
   herm = ishermitian(A) && ishermitian(B)
   return LinearOperator{T}(m * p, n * q, symm, herm, prod, tprod, ctprod)
 end
+
+kron(A :: AbstractMatrix, B :: AbstractLinearOperator) =
+    kron(LinearOperator(A), B)
+
+kron(A :: AbstractLinearOperator, B :: AbstractMatrix) =
+    kron(A, LinearOperator(B))
