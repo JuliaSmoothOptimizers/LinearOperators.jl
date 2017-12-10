a29bd37c0779bb571450537d39c4bb29c2ab305a
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2017-12-10 18:20:11 -0200

    Change kron implementation

--- test/test_kron.jl
+++ test/test_kron.jl
@@ -1,38 +1,35 @@
 for A in Any[rand(2, 3), rand(-3:3, 2, 3),
-             rand(2, 3) + im * rand(2, 3)]
+             rand(2, 3) + im * rand(2, 3),
+             sprand(10, 10, 0.1)]
   for B in Any[rand(2, 3), rand(-3:3, 2, 3),
-               rand(2, 3) + im * rand(2, 3)]
+               rand(2, 3) + im * rand(2, 3),
+               sprand(10, 10, 0.1)]
     K = kron(A, B)
-    T = opKron(A, B)
-    @test K == full(T)
-    @test K' == full(T')
-    @test K.' == full(T.')
-    m, n = size(K)
-    err = 0.0
-    for t = 1:100
-      x = rand(n)
-      Kx = K * x
-      Tx = T * x
-      err += norm(Kx - Tx)
+    T1 = kron(LinearOperator(A), B)
+    T2 = kron(A, LinearOperator(B))
+    T3 = kron(LinearOperator(A), LinearOperator(B))
+    for T in [T1, T2, T3]
+      @test K == full(T)
+      @test K' == full(T')
+      @test K.' == full(T.')
+      m, n = size(K)
+      err = 0.0
+      for t = 1:100
+        x = rand(n)
+        Kx = K * x
+        Tx = T * x
+        err += norm(Kx - Tx)
 
-      x = rand(m)
-      Kx = K' * x
-      Tx = T' * x
-      err += norm(Kx - Tx)
+        x = rand(m)
+        Kx = K' * x
+        Tx = T' * x
+        err += norm(Kx - Tx)
 
-      Kx = K.' * x
-      Tx = T.' * x
-      err += norm(Kx - Tx)
+        Kx = K.' * x
+        Tx = T.' * x
+        err += norm(Kx - Tx)
+      end
+      @test err < 1e-12
     end
-    @test err < 1e-12
   end
 end
-
-A = rand(5, 5)
-B = rand(5, 5)
-TB = LinearOperator(B)
-K = kron(A, B)
-T = opKron(A, TB)
-@test K == full(T)
-@test K' == full(T')
-@test K.' == full(T.')
