e106cf2b26aae9e4a09b2742b6edd2a2007a641b
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2017-12-27 15:04:23 -0200

    Use relative error on kron test

--- test/test_kron.jl
+++ test/test_kron.jl
@@ -5,13 +5,14 @@ for A in Any[rand(2, 3), rand(-3:3, 2, 3),
                rand(2, 3) + im * rand(2, 3),
                sprand(10, 10, 0.1)]
     K = kron(A, B)
+    normK = norm(K, 1)
     T1 = kron(LinearOperator(A), B)
     T2 = kron(A, LinearOperator(B))
     T3 = kron(LinearOperator(A), LinearOperator(B))
     for T in [T1, T2, T3]
-      @test norm(K - full(T)) < eps()
-      @test norm(K' - full(T')) < eps()
-      @test norm(K.' - full(T.')) < eps()
+      @test norm(K - full(T), 1) < eps() * normK
+      @test norm(K' - full(T'), 1) < eps() * normK
+      @test norm(K.' - full(T.'), 1) < eps() * normK
       m, n = size(K)
       err = 0.0
       for t = 1:100
