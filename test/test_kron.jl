4395a753aabb0340603a772aac80afbed02fcbad
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2018-04-11 18:19:51 -0300

    Use @testset

--- test/test_kron.jl
+++ test/test_kron.jl
@@ -1,36 +1,42 @@
-for A in Any[rand(2, 3), rand(-3:3, 2, 3),
-             rand(2, 3) + im * rand(2, 3),
-             sprand(10, 10, 0.1)]
-  for B in Any[rand(2, 3), rand(-3:3, 2, 3),
-               rand(2, 3) + im * rand(2, 3),
-               sprand(10, 10, 0.1)]
-    K = kron(A, B)
-    normK = norm(K, 1)
-    T1 = kron(LinearOperator(A), B)
-    T2 = kron(A, LinearOperator(B))
-    T3 = kron(LinearOperator(A), LinearOperator(B))
-    for T in [T1, T2, T3]
-      @test norm(K - full(T), 1) < eps() * normK
-      @test norm(K' - full(T'), 1) < eps() * normK
-      @test norm(transpose(K) - full(transpose(T)), 1) < eps() * normK
-      m, n = size(K)
-      err = 0.0
-      for t = 1:100
-        x = rand(n)
-        Kx = K * x
-        Tx = T * x
-        err += norm(Kx - Tx)
+function test_kron()
+  @testset "Kron" begin
+    for A in Any[rand(2, 3), rand(-3:3, 2, 3),
+                 rand(2, 3) + im * rand(2, 3),
+                 sprand(10, 10, 0.1)]
+      for B in Any[rand(2, 3), rand(-3:3, 2, 3),
+                   rand(2, 3) + im * rand(2, 3),
+                   sprand(10, 10, 0.1)]
+        K = kron(A, B)
+        normK = norm(K, 1)
+        T1 = kron(LinearOperator(A), B)
+        T2 = kron(A, LinearOperator(B))
+        T3 = kron(LinearOperator(A), LinearOperator(B))
+        for T in [T1, T2, T3]
+          @test norm(K - full(T), 1) < eps() * normK
+          @test norm(K' - full(T'), 1) < eps() * normK
+          @test norm(transpose(K) - full(transpose(T)), 1) < eps() * normK
+          m, n = size(K)
+          err = 0.0
+          for t = 1:100
+            x = rand(n)
+            Kx = K * x
+            Tx = T * x
+            err += norm(Kx - Tx)
 
-        x = rand(m)
-        Kx = K' * x
-        Tx = T' * x
-        err += norm(Kx - Tx)
+            x = rand(m)
+            Kx = K' * x
+            Tx = T' * x
+            err += norm(Kx - Tx)
 
-        Kx = transpose(K) * x
-        Tx = transpose(T) * x
-        err += norm(Kx - Tx)
+            Kx = transpose(K) * x
+            Tx = transpose(T) * x
+            err += norm(Kx - Tx)
+          end
+          @test err < 1e-12
+        end
       end
-      @test err < 1e-12
     end
   end
 end
+
+test_kron()
--- test/test_lsr1.jl
+++ test/test_lsr1.jl
@@ -2,63 +2,64 @@ function test_lsr1()
   ϵ = eps(Float64)
   rtol = sqrt(ϵ)
 
-  # test limited-memory SR1
-  n = 10
-  mem = 5
-  B = LSR1Operator(n, mem, scaling=false)
+  @testset "LSR1" begin
+    n = 10
+    mem = 5
+    B = LSR1Operator(n, mem, scaling=false)
 
-  for t = 1:2
-    @assert norm(diag(B) - diag(full(B))) <= rtol
+    for t = 1:2
+      @assert norm(diag(B) - diag(full(B))) <= rtol
 
-    @assert B.data.insert == 1
-    @test norm(full(B) - Matrix(1.0I, n, n)) <= ϵ
+      @assert B.data.insert == 1
+      @test norm(full(B) - Matrix(1.0I, n, n)) <= ϵ
 
-    # Test that only valid updates are accepted.
-    s = rand(n)
-    y = B * s
-    push!(B, s, y); @assert B.data.insert == 1
-
-    # Insert a few {s,y} pairs.
-    for i = 1 : mem+2
+      # Test that only valid updates are accepted.
       s = rand(n)
-      y = rand(n)
-      push!(B, s, y)
+      y = B * s
+      push!(B, s, y); @assert B.data.insert == 1
+
+      # Insert a few {s,y} pairs.
+      for i = 1 : mem+2
+        s = rand(n)
+        y = rand(n)
+        push!(B, s, y)
+      end
+
+      @test check_hermitian(B)
+      @assert norm(diag(B) - diag(full(B))) <= rtol
+
+      v = rand(n)
+      @assert norm(B * v - v) > rtol
+      reset!(B)
+      @assert norm(B * v - v) < rtol
     end
 
-    @test check_hermitian(B)
-    @assert norm(diag(B) - diag(full(B))) <= rtol
+    # test against full SR1 without scaling
+    mem = n
+    LB = LSR1Operator(n, mem, scaling=false)
+    B = Matrix(1.0I, n, n)
 
-    v = rand(n)
-    @assert norm(B * v - v) > rtol
-    reset!(B)
-    @assert norm(B * v - v) < rtol
-  end
-
-  # test against full SR1 without scaling
-  mem = n
-  LB = LSR1Operator(n, mem, scaling=false)
-  B = Matrix(1.0I, n, n)
-
-  function sr1!(B, s, y)
-    # dense SR1 update
-    ymBs = y - B * s
-    denom = dot(ymBs, s)
-    if abs(denom) >= 1.0e-8 + 1.0e-8 * norm(s) * norm(ymBs)
-      B = B + ymBs * ymBs' / denom
+    function sr1!(B, s, y)
+      # dense SR1 update
+      ymBs = y - B * s
+      denom = dot(ymBs, s)
+      if abs(denom) >= 1.0e-8 + 1.0e-8 * norm(s) * norm(ymBs)
+        B = B + ymBs * ymBs' / denom
+      end
+      return B
     end
-    return B
-  end
 
-  @assert norm(full(LB) - B) < rtol * norm(B)
-  @assert norm(diag(LB) - diag(B)) < rtol * norm(diag(B))
-
-  for k = 1 : mem
-    s = rand(n)
-    y = rand(n)
-    B = sr1!(B, s, y)
-    LB = push!(LB, s, y)
     @assert norm(full(LB) - B) < rtol * norm(B)
     @assert norm(diag(LB) - diag(B)) < rtol * norm(diag(B))
+
+    for k = 1 : mem
+      s = rand(n)
+      y = rand(n)
+      B = sr1!(B, s, y)
+      LB = push!(LB, s, y)
+      @assert norm(full(LB) - B) < rtol * norm(B)
+      @assert norm(diag(LB) - diag(B)) < rtol * norm(diag(B))
+    end
   end
 end
 
