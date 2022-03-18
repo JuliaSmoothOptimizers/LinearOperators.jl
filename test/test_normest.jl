7ce972379e2704d58b3a8ca7e3662d627c7d5c52
farhadrclass <31899325+farhadrclass@users.noreply.github.com>
2022-03-18 13:42:00 -0400

    added a test to set difference tolerance

--- test/test_normest.jl
+++ test/test_normest.jl
@@ -5,9 +5,9 @@ function test_normest()
   end
 end
 
-function simple_matrix_test_helper(S, ϵ)
+function simple_matrix_test_helper(S,ϵ_norm ,ϵ)
   val = opnorm(Matrix(S), 2)
-  val_normest, _ = normest(S, 1.0e-4, 10000)
+  val_normest, _ = normest(S, ϵ_norm, 10000)
   if val == 0
       dev = 1
   else
@@ -18,15 +18,17 @@ end
 
 function simple_matrix_test(ϵ)
   (nrow, ncol) = (10, 10)  
+  ϵ_norm = eps(Float64)
   A1 = simple_matrix(ComplexF64, nrow, ncol)
-  simple_matrix_test_helper(A1, ϵ)
+  simple_matrix_test_helper(A1, ϵ_norm,ϵ)
   LA1 = LinearOperator(A1)
-  simple_matrix_test_helper(LA1, ϵ)
+  simple_matrix_test_helper(LA1, ϵ_norm, ϵ)
 
   A2 = simple_matrix(Float64, nrow, ncol)
-  simple_matrix_test_helper(A2, ϵ)
+  simple_matrix_test_helper(A2, ϵ_norm, ϵ)
   LA2 = LinearOperator(A2)
-  simple_matrix_test_helper(LA2, ϵ)
+  ϵ_norm = -1
+  simple_matrix_test_helper(LA2,ϵ_norm, ϵ)
 end
 
 test_normest()
