9686d3319113c3cd41fe387e93cc27f3df331657
farhadrclass <31899325+farhadrclass@users.noreply.github.com>
2022-04-13 16:52:26 -0400

    Update utilities.jl

--- src/utilities.jl
+++ src/utilities.jl
@@ -34,10 +34,10 @@ function normest(S, tol = -1, maxiter = 100)
 
   x ./= e
   e_0 = zero(e)
-
+  Sx = zeros(eltype(S), n)
+  
   while abs(e - e_0) > tol * e
     e_0 = e
-    Sx = zeros(eltype(S), n)
     mul!(Sx, S, x)
     if count(x -> x != 0, Sx) == 0
       Sx .= randn(eltype(Sx), size(Sx))
