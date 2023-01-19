9b6131fcd19c5bf1f6e52842228d6a41996123c8
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-01-19 09:21:46 +0100

    Bump Documenter version

--- docs/make.jl
+++ docs/make.jl
@@ -5,10 +5,9 @@ makedocs(
   doctest = true,
   # linkcheck = true,
   strict = true,
-  format = Documenter.HTML(
-    assets = ["assets/style.css"],
-    prettyurls = get(ENV, "CI", nothing) == "true",
-  ),
+  format = Documenter.HTML(assets = ["assets/style.css"],
+                           ansicolor = true,
+                           prettyurls = get(ENV, "CI", nothing) == "true"),
   sitename = "LinearOperators.jl",
   pages = Any["Home" => "index.md", "Tutorial" => "tutorial.md", "Reference" => "reference.md"],
 )
