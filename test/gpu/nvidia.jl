d7e998069293b92fd00d5fbf925a1af9bbc78cd9
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-01-19 09:41:33 +0100

    Setup buildkite

---
+++ test/gpu/nvidia.jl
@@ -0,0 +1,16 @@
+using LinearOperators, CUDA, CUDA.CUSPARSE, CUDA.CUSOLVER
+
+@testset "Nvidia -- CUDA.jl" begin
+
+  @test CUDA.functional()
+  CUDA.allowscalar(false)
+
+  A = CUDA.rand(5,5)
+  B = CUDA.rand(10,10)
+  C = CUDA.rand(20,20)
+  M = BlockDiagonalOperator(A, B, C)
+
+  v = CUDA.rand(35)
+  y = M * v
+  @test y <: CuVector{Float32}
+end
