9f4bdf05aefb79ee197fdd2dea079783c629ba20
nHackel <nHackel@users.noreply.github.com>
2024-04-11 20:10:50 +0200

    Switch to CuArray storage_type + new default S value

--- ext/LinearOperatorsCUDAExt.jl
+++ ext/LinearOperatorsCUDAExt.jl
@@ -1,19 +1,8 @@
 module LinearOperatorsCUDAExt
 
-using LinearOperators, LinearOperators.FastClosures, LinearOperators.LinearAlgebra
+using LinearOperators
 isdefined(Base, :get_extension) ? (using CUDA) : (using ..CUDA)
 
-function LinearOperators.LinearOperator(
-  M::CuArray{T, 2, D};
-  symmetric = false,
-  hermitian = false,
-  S = CuArray{T, 1, D},
-) where {T, D}
-  nrow, ncol = size(M)
-  prod! = @closure (res, v, α, β) -> mul!(res, M, v, α, β)
-  tprod! = @closure (res, u, α, β) -> mul!(res, transpose(M), u, α, β)
-  ctprod! = @closure (res, w, α, β) -> mul!(res, adjoint(M), w, α, β)
-  LinearOperators.LinearOperator{T}(nrow, ncol, symmetric, hermitian, prod!, tprod!, ctprod!, S = S)
-end
+LinearOperators.storage_type(::CuArray{T, 2, D}) where {T, D} = CuArray{T, 1, D}
 
 end # module
