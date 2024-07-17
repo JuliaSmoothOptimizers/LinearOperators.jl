594d7d1ad0aea4cd45ed8d4299d9efc0ccc320ca
nHackel <nHackel@users.noreply.github.com>
2024-07-17 11:07:18 +0200

    Add storage_type for Sparse CuArrays

--- ext/LinearOperatorsCUDAExt.jl
+++ ext/LinearOperatorsCUDAExt.jl
@@ -1,8 +1,9 @@
 module LinearOperatorsCUDAExt
 
 using LinearOperators
-isdefined(Base, :get_extension) ? (using CUDA) : (using ..CUDA)
+isdefined(Base, :get_extension) ? (using CUDA; using CUDA.CUSPARSE) : (using ..CUDA; using ..CUDA.CUSPARSE)
 
 LinearOperators.storage_type(::CuArray{T, 2, D}) where {T, D} = CuArray{T, 1, D}
+LinearOperators.storage_type(::AbstractCuSparseMatrix{T}) where {T} = CuArray{T, 1, CUDA.DeviceMemory}
 
 end # module
