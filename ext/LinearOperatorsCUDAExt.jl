1a95b869ba88f8ac99a0914e013aa1b04fc8bd7e
dpo <dpo@users.noreply.github.com>
2024-10-09 23:07:58 +0000

    :robot: Format .jl files

--- ext/LinearOperatorsCUDAExt.jl
+++ ext/LinearOperatorsCUDAExt.jl
@@ -1,9 +1,11 @@
 module LinearOperatorsCUDAExt
 
 using LinearOperators
-isdefined(Base, :get_extension) ? (using CUDA; using CUDA.CUSPARSE) : (using ..CUDA; using ..CUDA.CUSPARSE)
+isdefined(Base, :get_extension) ? (using CUDA; using CUDA.CUSPARSE) :
+(using ..CUDA; using ..CUDA.CUSPARSE)
 
 LinearOperators.storage_type(::CuArray{T, 2, D}) where {T, D} = CuArray{T, 1, D}
-LinearOperators.storage_type(::AbstractCuSparseMatrix{T}) where {T} = CuArray{T, 1, CUDA.DeviceMemory}
+LinearOperators.storage_type(::AbstractCuSparseMatrix{T}) where {T} =
+  CuArray{T, 1, CUDA.DeviceMemory}
 
 end # module
