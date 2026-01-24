7750f0df643477ef02cd95147727f524a099f40b
farhadrclass <31899325+farhadrclass@users.noreply.github.com>
2026-01-24 18:53:42 -0500

    Update shifted_operators.jl

--- src/shifted_operators.jl
+++ src/shifted_operators.jl
@@ -42,7 +42,6 @@ function shifted_ctprod!(y, data::ShiftedData, x, α, β)
   mul!(y, adjoint(data.H), x, α, β)
 
   # y = y + (α * conj(σ)) * x
-  # Note: We must conjugate σ for the adjoint
   if !iszero(data.σ)
     axpy!(α * conj(data.σ), x, y)
   end
@@ -53,8 +52,7 @@ end
     ShiftedOperator(H, σ=0)
 
 Constructs a linear operator representing `op = H + σI`.
-This implementation uses closures to handle multiplication, allowing full compatibility 
-with jl's generic methods.
+
 """
 mutable struct ShiftedOperator{T, OpH, F, Ft, Fct} <: AbstractLinearOperator{T}
   nrow::Int
@@ -98,24 +96,17 @@ end
 
 function ShiftedOperator(H::OpH, σ::T = zero(eltype(H))) where {T, OpH}
 
-  # 1. Create the Data object
   data = ShiftedData(H, σ)
-
-  # 2. Define Closures
-  # We capture `data` so that if `data.σ` is mutated, the operator updates automatically.
 
   prod! = (y, x, α, β) -> shifted_prod!(y, data, x, α, β)
   tprod! = (y, x, α, β) -> shifted_tprod!(y, data, x, α, β)
   ctprod! = (y, x, α, β) -> shifted_ctprod!(y, data, x, α, β)
 
-  # 3. Determine properties
   n = size(H, 1)
-  # Symmetry: H must be symmetric. σI is always symmetric.
+
   is_sym = issymmetric(H)
-  # Hermitian: H must be Hermitian AND σ must be real.
   is_herm = ishermitian(H) && isreal(σ)
 
-  # 4. Return the Operator
   return ShiftedOperator(n, n, is_sym, is_herm, prod!, tprod!, ctprod!, data)
 end
 
@@ -123,12 +114,9 @@ size(op::ShiftedOperator) = (op.nrow, op.ncol)
 issymmetric(op::ShiftedOperator) = op.symmetric
 ishermitian(op::ShiftedOperator) = op.hermitian
 
-# required to use the generic 5-argument mul! logic from the library
 has_args5(op::ShiftedOperator) = true
 use_prod5!(op::ShiftedOperator) = true
 
-# We return true for isallocated5 because we don't need the library 
-# to allocate temporary vectors for us; we delegate everything to `op.data.H`.
 isallocated5(op::ShiftedOperator) = true
 
 storage_type(op::ShiftedOperator{T}) where {T} = Vector{T}
